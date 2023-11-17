b63bbd3ece0ea475a19564c48fa6aac80f318c5b
Mahmoud Korim Kandel <69222454+mahmoudkandel980@users.noreply.github.com>
2023-11-17 19:26:54 +0200

    create contants ( colors )

---
+++ src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+    lightDark: "#333533",
+    darkRed: "#e03131",
+    lightRed: "#ff6b6b",
+    darkGreen: "#37b24d",
+    lightBlue: "#1DA1F2",
+    lighertBlue: "#74c0fc",
+    lightGray: "#adb5bd",
+    white: "#fff",
+};
