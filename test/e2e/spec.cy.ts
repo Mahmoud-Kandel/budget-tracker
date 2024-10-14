cdccc5b71f1c41e3ad8865c2bfc2be7f687fa5a2
Mahmoud Korim Kandel <kandel@braina.live>
2024-10-14 19:23:02 +0300

    console log some env vars

--- test/e2e/spec.cy.ts
+++ test/e2e/spec.cy.ts
@@ -8,5 +8,8 @@ describe("example to-do app", () => {
 
     it("displays two todo items by default", () => {
         cy.get(`#${elementsId.summaryChartsContainer}`).should("exist");
+        console.log(import.meta.env.VITE_QRCODE_HOST);
+        console.log(import.meta.env.VITE_QRCODE_CHAIN_ID);
+        console.log(import.meta.env.VITE_BACKEND_URL);
     });
 });
