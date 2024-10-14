0252f0e6a9a86462f6dfa5886247a8a7da123833
Mahmoud Korim Kandel <kandel@braina.live>
2024-10-14 19:29:30 +0300

    create some logs

--- test/e2e/spec.cy.ts
+++ test/e2e/spec.cy.ts
@@ -7,9 +7,8 @@ describe("example to-do app", () => {
     });
 
     it("displays two todo items by default", () => {
+        console.log("import.meta.env.VITE_QRCODE_HOST");
+        console.log(import.meta.env.VITE_QRCODE_HOST);
         cy.get(`#${elementsId.summaryChartsContainer}`).should("exist");
-        // console.log(import.meta.env.VITE_QRCODE_HOST);
-        // console.log(import.meta.env.VITE_QRCODE_CHAIN_ID);
-        // console.log(import.meta.env.VITE_BACKEND_URL);
     });
 });
